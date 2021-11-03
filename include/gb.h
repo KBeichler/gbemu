c79f2e793714b6ec95c9f6fc965b41f3dd9adf43
Kevin <k.beichler@gmx.com>
2021-11-03 20:53:09 +0100

    start setting up memory

--- include/gb.h
+++ include/gb.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <cpu.h>
+#include <mem.h>
 
 
 
--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <gb.h>
 
 
-int main(void){
+int main (int argc, char* argv[]) 
+{
+    
+    if (mem_loadRom(argv[1]))
+    {
+        return 0;
+    }
 
+    
     printf("Hello GB \n");
 
     cpu_t c;
     c.HC = 1;
     printf("AF Register %x\n", c.AF);
+
     cpu_init();
-
+    mem_init();
+    mem_close();
+ 
     return 0;
+    
 
-}
+} 
